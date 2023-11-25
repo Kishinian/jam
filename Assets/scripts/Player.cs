c68ecbbcec05cc80f036f80e5cef0de0cf09f659
Kishinian <glaziyrinaksenia.84@gmail.com>
2023-11-25 22:30:13 +0500

    8
    передвижение робота
    добавление тегов

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class playercontrol : MonoBehaviour
 {
+    private new Vector2 targetpos;
+    public float Yincrement;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -12,7 +17,18 @@ public class playercontrol : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+
     {
-        
+        if (Input.GetKey(KeyCode.W)) 
+        {
+            targetpos = new Vector2(transform.position.x, transform.position.y + Yincrement);
+            transform.position = targetpos;
+        }
+
+        else if (Input.GetKey(KeyCode.S))
+        {
+            targetpos = new Vector2(transform.position.x, transform.position.y - Yincrement);
+            transform.position = targetpos;
+        }
     }
 }

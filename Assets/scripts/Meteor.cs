996ca194a0b206392f407bf9f4647c8984da1f87
Kishinian <glaziyrinaksenia.84@gmail.com>
2023-11-25 21:06:15 +0500

    7
    метеор

--- Assets/scripts/Meteor.cs
+++ Assets/scripts/Meteor.cs
@@ -4,9 +4,21 @@ using UnityEngine;
 
 public class Meteor : MonoBehaviour
 {
-    
-    void Update()
+    public float speed = 5f; // Скорость движения препятствия
+
+    private void Update()
     {
-        
+        transform.Translate(Vector2.left * speed * Time.deltaTime); // Движение препятствия вперед
     }
+    // Проверка столкновения с игроком
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // Уменьшение скорости движения препятствия
+            speed *= 0.8f;
+            Destroy(gameObject);
+        }
+    }
+
 }
